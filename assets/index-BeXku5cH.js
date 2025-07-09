ec8ff6dd64ce863b59f71a618873a1ec49f0be7a
To-oDev <To-oDev@users.noreply.github.com>
2025-07-09 05:27:52 +0000

    deploy: 8121fab72c10d5565afbea6a9300387729d39701

--- assets/index-BeXku5cH.js
+++ assets/index-BeXku5cH.js
@@ -3943,7 +3943,7 @@ No matching component was found for:
   uniform float scrollY;
 
   float gridLine(float coord) {
-    float thickness = 0.0001;
+    float thickness = 0.001;
     return smoothstep(thickness, 0.0, abs(fract(coord * 5.0) - 0.5));
   }
 
